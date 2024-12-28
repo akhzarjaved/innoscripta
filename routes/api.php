957405d7b5bcd06a8364ff7b07e8f0a32a82b589
Akhzar Javed <akhzarjaved1993@gmail.com>
2024-12-28 14:30:58 +0500

    updated api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
@@ -12,5 +13,12 @@ Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('p
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('user', [AuthController::class, 'user']);
+    Route::get('articles', [ArticleController::class, 'search']);
+    Route::get('preferred-articles', [ArticleController::class, 'preferred']);
+
+    Route::group(['prefix' => 'user', 'controller' => UserController::class], function () {
+        Route::get('/', 'me');
+        Route::get('preferences', 'preferences');
+        Route::post('preferences', 'savePreferences');
+    });
 });
